43904314565c3d8c3ac5fcc3d3ecb5da5904fa11
efxxkw <gustavo941419@gmail.com>
2025-01-29 19:42:06 -0300

    Update speedxhubxx.lua

--- speedxhubxx.lua
+++ speedxhubxx.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/JhMqGGfF", true))()
 wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/efxxkw/speedxhub/refs/heads/main/speedxhub.lua", true))()
+loadstring(game:HttpGet("https://pastebin.com/W8avg6wb", true))()
