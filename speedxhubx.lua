e75fce0760d3e4a1e4cac53e98ca211b0ef7d98a
efxxkw <gustavo941419@gmail.com>
2025-01-29 17:42:51 -0300

    Add files via upload

---
+++ speedxhubx.lua
@@ -0,0 +1,7 @@
+spawn(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/JhMqGGfF, true"))()
+end)
+
+spawn(function()
+   loadstring(game:HttpGet(https://raw.githubusercontent.com/efxxkw/speedxhub/refs/heads/main/speedxhub.lua, true))()
+end)
